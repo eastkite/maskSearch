576aa39131bcf933e0d54935d01a3bbae1de50db
eastkite <eastkite13@gmail.com>
2020-03-11 19:57:18 +0900

    docker Set

--- docker/firstStart.sh
+++ docker/firstStart.sh
@@ -11,13 +11,8 @@ if [ -f server.pid ]; then
     kill -9 `cat server.pid`
 fi
 
-if [ -f article.pid ]; then
-    kill -9 `cat article.pid`
-fi
 
 nohup python3 run.py > crawling.out 2>&1 &
 echo $! > crawling.pid
 nohup python3 ServerRun.py > server.out 2>&1 &
 echo $! > server.pid
-nohup python3 newArticle.py > article.out 2>&1 &
-echo $! > article.pid
